f71b711195a294382f0def8ffdd1c36cc874f8e2
mondeepskz <110476494+mondeepskz@users.noreply.github.com>
2023-10-04 17:47:11 +0530

    Delete gptfront directory

--- gptfront/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Article } from './article/Article';
-export { default as Brand } from './brand/Brand';
-export { default as Cta } from './cta/Cta';
-export { default as Features } from './features/Features';
-export { default as Navbar } from './navbar/Navbar';
